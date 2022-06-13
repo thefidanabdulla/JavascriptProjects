23913ec6de180b647633ec1d7fdc83c7e6ade4e2
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-13 16:01:58 +0400

    Palindrome Checker is done

---
+++ Palindrome Checker/js/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('.btn');
+const input_value = document.querySelector('.input-text');
+const displayMessage = document.querySelector('.displayMessage');
+
+btn.addEventListener('click', checkPalindrome);
+
+function checkPalindrome(){
+    
+    let word = input_value.value;
+    let len = word.length;
+    let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+    let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+    let flip = [...end].reverse().join('');
+    if(start == flip){
+        displayMessage.innerHTML = `${word.toUpperCase()} is PALINDROME :)`;
+    }else{
+        displayMessage.innerHTML = `${word.toUpperCase()} is NOT PALINDROME :/`;
+    }
+    // window.alert(end)
+
+    input_value.value = ' ';
+}
