709cb4c917fcc5011c408cf650ada6dac6c1a79e
thefidanabdulla <thefidanabdulla@gmail.com>
2022-06-18 18:48:41 +0400

    tic tac toe game starting

--- Palindrome Checker/js/app.js
+++ Palindrome Checker/js/app.js
@@ -4,16 +4,16 @@ const displayMessage = document.querySelector('.displayMessage');
 
 btn.addEventListener('click', checkPalindrome);
 
-function checkPalindrome(){
-    
+function checkPalindrome() {
+
     let word = input_value.value;
     let len = word.length;
     let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
     let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
     let flip = [...end].reverse().join('');
-    if(start == flip){
+    if (start == flip) {
         displayMessage.innerHTML = `${word.toUpperCase()} is PALINDROME :)`;
-    }else{
+    } else {
         displayMessage.innerHTML = `${word.toUpperCase()} is NOT PALINDROME :/`;
     }
     // window.alert(end)
