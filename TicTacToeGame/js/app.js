e62f6134829a43a47acd08d3a6bec81321a84bee
thefidanabdulla <thefidanabdulla@gmail.com>
2022-07-01 21:06:43 +0400

    .

--- TicTacToeGame/js/app.js
+++ TicTacToeGame/js/app.js
@@ -1,9 +1,13 @@
 const selectBox = document.querySelector(".select-box");
 const selectXBtn = document.querySelector(".playerX");
 const selectOBtn = document.querySelector(".playerO");
-const playBoard = document.querySelector(".play-board")
+const playBoard = document.querySelector(".play-board");
+const allBox = document.querySelectorAll(".box");
+const players = document.querySelector(".players");
+const playArea = document.querySelector(".play-area")
 
 window.onload = () => {
+
     selectXBtn.onclick = () => {
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
@@ -11,5 +15,18 @@ window.onload = () => {
     selectOBtn.onclick = () => {
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
+        players.setAttribute("class", "players active player");
     }
+}
+
+playArea.addEventListener("click", clickedBox())
+
+function clickedBox(event){
+    // if(players.classList.contains("player")){
+    //      .innerHTML = "O"
+    // }else{
+    //      .innerHTML = "X" 
+    // }
+    // let a = event.target.value;
+    console.log("salam")
 }
